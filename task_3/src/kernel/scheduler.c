8fb311d3c22cbcb29dfd42bac39d4bd3957e8ff3
Flemming Madsen <mail@leflings.dk>
2013-02-18 08:35:53 +0100

    Skeleton for task 3

---
+++ task_3/src/kernel/scheduler.c
@@ -0,0 +1,17 @@
+/*!
+ * \file scheduler.c
+ * \brief
+ *  This file holds the scheduler code. 
+ */
+
+#include "kernel.h"
+
+void
+scheduler_called_from_system_call_handler(const register int schedule)
+{
+}
+
+void
+scheduler_called_from_timer_interrupt_handler(const register int thread_changed)
+{
+}
