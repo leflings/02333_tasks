14827a77ad17f3be9f13a91cdb793824bd47d1c2
Flemming Madsen <mail@leflings.dk>
2013-02-18 08:43:15 +0100

    Task 3 set up

--- task_3/src/kernel/syscall.c
+++ task_3/src/kernel/syscall.c
@@ -38,6 +38,66 @@ system_call_implementation(void)
    break;
   }
 
+	case SYSCALL_VERSION: {
+		SYSCALL_ARGUMENTS.rax = KERNEL_VERSION;
+		break;
+	}
+
+	case SYSCALL_CREATEPROCESS: {
+		int process_number, thread_number;
+		long int executable_number = SYSCALL_ARGUMENTS.rdi;
+		struct prepare_process_return_value prepare_process_ret_val;
+
+		for (process_number = 0; process_number < MAX_NUMBER_OF_PROCESSES && process_table[process_number].threads > 0; process_number++) {
+		}
+		prepare_process_ret_val = prepare_process(
+				executable_table[executable_number].elf_image,
+				process_number,
+				executable_table[executable_number].memory_footprint_size);
+
+		if(0 == prepare_process_ret_val.first_instruction_address) {
+			kprints("Error starting image\n");
+		}
+
+		process_table[process_number].parent = thread_table[current_thread].data.owner;
+
+		thread_number = allocate_thread();
+
+		thread_table[thread_number].data.owner = process_number;
+		thread_table[thread_number].data.registers.integer_registers.rflags = 0;
+		thread_table[thread_number].data.registers.integer_registers.rip = prepare_process_ret_val.first_instruction_address;
+
+		process_table[process_number].threads += 1;
+
+		SYSCALL_ARGUMENTS.rax = ALL_OK;
+
+		current_thread = thread_number;
+
+		break;
+	}
+	case SYSCALL_TERMINATE:
+	{
+		int i;
+		int owner_process = thread_table[current_thread].data.owner;
+		int parent_process = process_table[owner_process].parent;
+
+		thread_table[current_thread].data.owner = -1; /* Terminate Thread */
+
+		process_table[owner_process].threads -= 1; /* Decrement Thread count */
+
+		if(process_table[owner_process].threads < 1) {
+			cleanup_process(owner_process);
+		}
+
+		for(i=0; i < MAX_NUMBER_OF_THREADS && thread_table[i].data.owner != parent_process; i++) {
+		}
+		current_thread = i;
+
+		break;
+
+	}
+
+
   /* Do not touch any lines above or including this line. */
 
   /* Add the implementation of more system calls here. */
